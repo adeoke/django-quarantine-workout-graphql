8a04d5a49d2b5119e73f7a63261c1f43a64bf75a
adeoke <adesmail100@gmail.com>
2020-05-29 00:45:33 +0100

    wip renaming classes and methods, also removed degubbing statements

--- quarantineworkout/stars/models.py
+++ quarantineworkout/stars/models.py
@@ -2,7 +2,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
-# TODO: to test minimum validation.
 class Star(models.Model):
     number = models.IntegerField(validators=[MinValueValidator(1),
                                              MaxValueValidator(5)])
