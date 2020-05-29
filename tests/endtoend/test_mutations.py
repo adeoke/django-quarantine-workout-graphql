3b614af222e4f98e2cc35c736df093ad64a4b1fd
adeoke <adesmail100@gmail.com>
2020-05-29 03:39:12 +0100

    template for token test added

--- tests/endtoend/test_mutations.py
+++ tests/endtoend/test_mutations.py
@@ -13,7 +13,31 @@ class TestAppMutations(unittest.TestCase):
             endpoint="{}{}".format(self.host, self.path))
 
     def test_create_user_returns_token_in_data_response(self):
-        pass
+        # step 1 create the user
+        # user_field = GqlQuery().fields(['username', 'email', 'id']).query(
+        #     name='user').generate()
+        #
+        # query = GqlQuery().fields([user_field]).query("createUser", input={
+        #     "email": "\"anemail4@example.com\"",
+        #     "password": "\"password123\"",
+        #     "username": "\"auserisnamed4\""}).operation("mutation"
+        #                                             ).generate()
+        # print(query)
+        # data = self.client.execute(query=query)
+        # print(data)
+
+        # step 2 use the user to authenticate
+
+        query = GqlQuery().fields(['token']).query("tokenAuth", input={"password": "\"password123\"",
+        "username": "\"auserisnamed4\""}).operation("mutation", name="").generate()
+
+        data = self.client.execute(query=query)
+        print(data)
+
+        # authenticate that the token is returned.
+
+
+
 
 # client info
 # https://stackoverflow.com/questions/48693825/making-a-graphql-mutation-from-my-python-code-getting-error
